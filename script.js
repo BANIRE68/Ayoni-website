d79456f6c40cbab49b9d6820b68511442a430b5b
BANIRE68 <adelekemuiz3779@gmail.com>
2026-08-14 21:50:22 +0100

    Initial website
    AYONI WEBSITE

---
+++ script.js
@@ -0,0 +1,8 @@
+const surpriseBtn = document.getElementById("surpriseBtn");
+const surprisedMessage = document.getElementById("surpriseMessage");
+
+surpriseBtn.addEventListener("click", function () {
+
+    surprisedMessage.textContent = 
+    "You are genuinely one of the most special people in my life. ❤️";
+});
